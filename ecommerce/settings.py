a9c29de8524139991ee41c0603b3cb2783213f42
andimajid24 <abdimajidmohamed55@gmail.com>
2022-09-28 10:03:50 +0300

    add database in setting.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -98,8 +98,10 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': config('djangoecommerce'),
+           'USER': config('postgres'),
+           'PASSWORD': config('12345'),
     }
 }
 
