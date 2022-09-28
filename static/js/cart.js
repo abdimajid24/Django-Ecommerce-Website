b36e82faf0eac1715f7af7431a5ed2df295fd9b5
andimajid24 <abdimajidmohamed55@gmail.com>
2022-09-28 17:54:29 +0300

    add price_total

--- static/js/cart.js
+++ static/js/cart.js
@@ -8,34 +8,61 @@ for (i = 0; i < updateBtns.length; i++) {
 		console.log('USER:', user)
 
 		if (user == 'AnonymousUser'){
-			console.log('User is not authenticated')
-			
+			addCookieItem(productId, action)
 		}else{
 			updateUserOrder(productId, action)
 		}
 	})
 }
 
-function updateUserOrder(productId, action){
 
-	console.log('User is authenticated, sending data...')
+function addCookieItem(productId, action){
+	console.log('User is not authenticated')
 
-    var url = '/update_item/'
+	if (action == 'add'){
+		if (cart[productId] == undefined){
+            cart[productId] = {'quantity':1}
 
-    fetch(url, {
-        method:'POST',
-        headers:{
-            'Content-Type':'application/json',
-            'X-CSRFToken': csrftoken
-        }, 
-        body:JSON.stringify({'productId':productId, 'action':action})
-    })
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        console.log('Data:', data)
-        location.reload()
-    });
+		}else{
+			cart[productId]['quantity'] += 1
+		}
+	}
+
+	if (action == 'remove'){
+		cart[productId]['quantity'] -= 1
+
+		if (cart[productId]['quantity'] <= 0){
+			console.log('Item should be deleted')
+			delete cart[productId];
+		}
+	}
+	console.log('CART:', cart)
+	document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
+	
+	location.reload()
 }
 
+
+
+
+
+function updateUserOrder(productId, action){
+	console.log('User is authenticated, sending data...')
+
+		var url = '/update_item/'
+
+		fetch(url, {
+			method:'POST',
+			headers:{
+				'Content-Type':'application/json',
+				'X-CSRFToken':csrftoken,
+			}, 
+			body:JSON.stringify({'productId':productId, 'action':action})
+		})
+		.then((response) => {
+		   return response.json();
+		})
+		.then((data) => {
+		    location.reload()
+		});
+}
